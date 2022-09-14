3cded2aa2d8265cd118d62192bca8cc847b56c80
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-09-14 12:53:21 +0200

    added file with data types

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const fs = require("fs");
+import fs from "fs";
 import axios from "axios";
 
-for (let i = 0; i <= 151; i++) {}
+for (let i = 1; i <= 151; i++) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+  const { data } = await axios.get(url);
+  //Get the types type.name[0] and assign it to a variable
+  const { id, types } = data;
+
+  const dataToStore = {
+    id,
+    types: types.map((type) => type.type.name),
+  };
+  //Store the dataToStore in the dataTypes.json file
+  fs.appendFile(
+    "dataTypes.json",
+    JSON.stringify(dataToStore) + ",",
+    (err) => err && console.log(err)
+  );
+}
